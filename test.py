5afe39d7d6982d75d95022bc212fb63360d5b2d3
kwangwoon001 <cnd820@gmail.com>
2023-03-30 16:47:06 +0900

    Revert "Added print_bye function"
    This reverts commit 0a90ab66a02a2d8a71451b153664d0fb3cf22c00.

--- test.py
+++ test.py
@@ -2,10 +2,6 @@ def print_hello(amount):
     for i in range(amount):
         print("Line {}: Hello World!".format(i + 1))
 
-def print_bye():
-    print("Good Bye...")
-
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     print_hello(num)
-    print_bye()
