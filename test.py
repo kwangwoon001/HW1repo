310cf75fcecfd3dc78a165d701aa15fb629b2f80
kwangwoon001 <cnd820@gmail.com>
2023-03-30 16:52:40 +0900

    print_bye added

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 def print_hello(amount):
-    for i in range(amount):
-        print("Line {}: Hello World!".format(i + 1))
+    for _ in range(amount):
+        print("Hello World!")
+
+def print_bye():
+    print("Good Bye...")
 
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     print_hello(num)
+    print_bye()
